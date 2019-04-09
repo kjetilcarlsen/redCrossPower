74250b386728ef6df27c12f3255e7c1c51ac14de
Reinhartsen <ole-christian.reinhartsen@capgemini.com>
2019-04-09 14:53:33 +0200

    Added iframes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+function useWindowSize() {
+  const w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0],
+    x = w.innerWidth || e.clientWidth || g.clientWidth,
+    y = w.innerHeight || e.clientHeight || g.clientHeight;
+
+
+  return { x, y }
+}
+
+const size = {
+  mobile: 480,
+  tablet: 768,
+  web: 1249
+}
+
+
+function App() {
+  const [{ x, y }, setWindowSize] = React.useState(useWindowSize())
+
+  React.useEffect(() => {
+
+    function resizeWindow() {
+      setWindowSize(useWindowSize())
+    }
+
+    window.addEventListener("resize", resizeWindow)
+
+    return () => {
+      window.removeEventListener("resize", resizeWindow);
+    }
+  })
+
+  function renderIframeSrc() {
+    if (x > size.mobile) {
+      return "https://app.powerbi.com/view?r=eyJrIjoiYzFjYWFlNzQtMzBhOS00MzExLWI0MzQtNDliYjg4ZjEzYTg1IiwidCI6Ijc2YTJhZTVhLTlmMDAtNGY2Yi05NWVkLTVkMzNkNzdjNGQ2MSIsImMiOjh9"
+    } else if (x > size.tablet) {
+      return "https://app.powerbi.com/view?r=eyJrIjoiODIwYWY2MGEtYzRmMy00MTkyLTgwY2QtYjM1MWQxOGNkOTBlIiwidCI6Ijc2YTJhZTVhLTlmMDAtNGY2Yi05NWVkLTVkMzNkNzdjNGQ2MSIsImMiOjh9"
+    } else {
+      return "https://app.powerbi.com/view?r=eyJrIjoiODIwYWY2MGEtYzRmMy00MTkyLTgwY2QtYjM1MWQxOGNkOTBlIiwidCI6Ijc2YTJhZTVhLTlmMDAtNGY2Yi05NWVkLTVkMzNkNzdjNGQ2MSIsImMiOjh9"
+
+    }
   }
+
+  return (
+    <div className="App">
+      <iframe title="power-bi-redcross" width={x} height={y} src={renderIframeSrc()} frameborder="0" allowFullScreen="true" />
+    </div>
+  );
 }
 
 export default App;
