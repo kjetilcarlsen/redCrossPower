f9000351ca420a73498f697f1b852e6a06d93c74
reinole <reinhartsen.oc@gmail.com>
2019-05-22 15:08:43 +0200

    Added new url

--- src/App.js
+++ src/App.js
@@ -2,59 +2,66 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function useWindowSize() {
-  const w = window,
-    d = document,
-    e = d.documentElement,
-    g = d.getElementsByTagName('body')[0],
-    x = w.innerWidth || e.clientWidth || g.clientWidth,
-    y = w.innerHeight || e.clientHeight || g.clientHeight;
+	const w = window,
+		d = document,
+		e = d.documentElement,
+		g = d.getElementsByTagName('body')[0],
+		x = w.innerWidth || e.clientWidth || g.clientWidth,
+		y = w.innerHeight || e.clientHeight || g.clientHeight;
 
-  return { x, y }
+	return { x, y };
 }
 
 const size = {
-  mobile: 480,
-  tablet: 768,
-  web: 1249
-}
-
+	mobile: 480,
+	tablet: 768,
+	web: 1249
+};
 
 function App() {
-  const [{ x, y }, setWindowSize] = useState(useWindowSize())
+	const [{ x, y }, setWindowSize] = useState(useWindowSize());
 
- 
-  useEffect(() => {
+	useEffect(() => {
+		function resizeWindow() {
+			setWindowSize(useWindowSize());
+		}
 
-    function resizeWindow() {
-      setWindowSize(useWindowSize())
-    }
+		window.addEventListener('resize', resizeWindow);
 
-    window.addEventListener("resize", resizeWindow)
+		return () => {
+			window.removeEventListener('resize', resizeWindow);
+		};
+	}, [x, y]);
 
-    return () => {
-      window.removeEventListener("resize", resizeWindow);
-    }
-  }, [x, y])
+	function renderIframeSrc() {
+		const desktopEmbedUrl =
+			'https://app.powerbi.com/view?r=eyJrIjoiMjQzNzVmMjEtOGFjZi00ZDA0LWEwNDYtNjc2OWYxN2JlMzM0IiwidCI6Ijc2YTJhZTVhLTlmMDAtNGY2Yi05NWVkLTVkMzNkNzdjNGQ2MSIsImMiOjh9';
+		const tabletEmbedUrl =
+			'https://app.powerbi.com/view?r=eyJrIjoiMjQzNzVmMjEtOGFjZi00ZDA0LWEwNDYtNjc2OWYxN2JlMzM0IiwidCI6Ijc2YTJhZTVhLTlmMDAtNGY2Yi05NWVkLTVkMzNkNzdjNGQ2MSIsImMiOjh9';
+		const mobileEmbedUrl =
+			'https://app.powerbi.com/view?r=eyJrIjoiMjQzNzVmMjEtOGFjZi00ZDA0LWEwNDYtNjc2OWYxN2JlMzM0IiwidCI6Ijc2YTJhZTVhLTlmMDAtNGY2Yi05NWVkLTVkMzNkNzdjNGQ2MSIsImMiOjh9';
 
-  function renderIframeSrc() {
-    const desktopEmbedUrl = 'https://app.powerbi.com/view?r=eyJrIjoiYzFjYWFlNzQtMzBhOS00MzExLWI0MzQtNDliYjg4ZjEzYTg1IiwidCI6Ijc2YTJhZTVhLTlmMDAtNGY2Yi05NWVkLTVkMzNkNzdjNGQ2MSIsImMiOjh9';
-    const tabletEmbedUrl = 'https://app.powerbi.com/view?r=eyJrIjoiODIwYWY2MGEtYzRmMy00MTkyLTgwY2QtYjM1MWQxOGNkOTBlIiwidCI6Ijc2YTJhZTVhLTlmMDAtNGY2Yi05NWVkLTVkMzNkNzdjNGQ2MSIsImMiOjh9';
-    const mobileEmbedUrl = 'https://app.powerbi.com/view?r=eyJrIjoiODIwYWY2MGEtYzRmMy00MTkyLTgwY2QtYjM1MWQxOGNkOTBlIiwidCI6Ijc2YTJhZTVhLTlmMDAtNGY2Yi05NWVkLTVkMzNkNzdjNGQ2MSIsImMiOjh9';
+		if (x > size.web) {
+			return desktopEmbedUrl;
+		} else if (x > size.tablet && x < size.web) {
+			return tabletEmbedUrl;
+		} else {
+			return mobileEmbedUrl;
+		}
+	}
 
-    if (x > size.web) {
-      return desktopEmbedUrl;
-    } else if (x > size.tablet && x < size.web) {
-      return tabletEmbedUrl;
-    } else {
-      return mobileEmbedUrl;
-    }
-  }
-
-  return (
-    <div className="App">
-      <iframe title="power-bi-redcross" width={x} height={y} src={renderIframeSrc()} frameborder="0" allowFullScreen="true" />
-    </div>
-  );
+	return (
+		<div className='App'>
+			<iframe
+				title='power-bi-redcross'
+				width={x}
+				height={y}
+				src={renderIframeSrc()}
+				frameborder='0'
+				allowFullScreen='true'
+			/>
+		</div>
+	);
 }
 
 export default App;
